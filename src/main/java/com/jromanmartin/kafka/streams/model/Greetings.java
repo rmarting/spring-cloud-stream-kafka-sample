6d3633e45aafd9340a59a8c6b9f4c8a06bcc4459
Roman Martin <rmarting@redhat.com>
2019-04-08 10:42:22 +0200

    Migrated to Spring Boot 2 and Spring Cloud Stream Greenwich.

--- src/main/java/com/jromanmartin/kafka/streams/model/Greetings.java
+++ src/main/java/com/jromanmartin/kafka/streams/model/Greetings.java
@@ -11,17 +11,13 @@ public class Greetings {
 
     private long timestamp;
     private String message;
-    private int partition;
-    private long offset;
 
     public Greetings() {
     }
 
-    public Greetings(long timestamp, String message, int partition, long offset) {
+    public Greetings(long timestamp, String message) {
         this.timestamp = timestamp;
         this.message = message;
-        this.partition = partition;
-        this.offset = offset;
     }
 
 }
--- src/main/java/com/jromanmartin/kafka/streams/service/GreetingsListener.java
+++ src/main/java/com/jromanmartin/kafka/streams/service/GreetingsListener.java
@@ -4,16 +4,21 @@ import com.jromanmartin.kafka.streams.model.Greetings;
 import com.jromanmartin.kafka.streams.stream.GreetingsStreams;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 @Component
 @Slf4j
 public class GreetingsListener {
 
     @StreamListener(GreetingsStreams.INPUT)
-    public void handleGreetings(@Payload Greetings greetings) {
-        log.info("Received greetings: {}", greetings);
+    public void handleGreetings(@Payload Greetings greetings, @Headers Map<String, Object> headers) {
+        log.info("Received greetings: {}. Partition: {}. Offset: {}", greetings,
+                headers.get(KafkaHeaders.RECEIVED_PARTITION_ID), headers.get(KafkaHeaders.OFFSET));
     }
 
 }
